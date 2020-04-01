84ba93ec1c37e190e9db33bb3dc873841a8959e2
RobertoVergaraC <ravergara@miuandes.com>
2020-04-01 16:48:14 -0300

    Preparation

---
+++ Laboratorio3RobertoVergaraC/Laboratorio3RobertoVergaraC/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio3RobertoVergaraC
+{
+    class Producto
+    {
+        private string name;
+        private string brand;
+        private int price;
+        private int stock;
+
+        public Producto(string name, string brand, int price, int stock)
+        {
+            this.name = name;
+            this.brand = brand;
+            this.price = price;
+            this.stock = stock;
+        }
+    }
+}
---
+++ Laboratorio3RobertoVergaraC/Laboratorio3RobertoVergaraC/Ventas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio3RobertoVergaraC
+{
+    class Ventas
+    {
+        private List<Producto> products;
+    }
+}
